89546f6c19308ee203b912d92384c380d12e6287
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2022-08-08 13:06:55 +0900

    Update 기능 개발.py

--- 기능 개발/기능 개발.py
+++ 기능 개발/기능 개발.py
@@ -1,1 +1,20 @@
-
+def solution(progresses, speeds):
+    answer = []
+    days = []
+    for progress, speed in zip(progresses, speeds):
+        day = (100 - progress) // speed
+        if (100 - progress) % speed > 0: day += 1
+        days.append(day)
+    count = 0
+    tmp = 0
+    for idx in range(len(progresses)):
+        if days[tmp] >= days[idx]:
+            print('count')
+            count += 1
+        else:
+            tmp = idx
+            print('reset')
+            answer.append(count)
+            count = 1
+    answer.append(count)
+    return answer
